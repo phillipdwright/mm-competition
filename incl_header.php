8e3a221c7a01002d442afd60815856c17e14a318
Phil Wright <wright.phillip@gmail.com>
2020-03-10 15:07:10 -0600

    Include Bootstrap 4 in the project

--- incl_header.php
+++ incl_header.php
@@ -1,7 +1,10 @@
+<!doctype html>
 <html>
-    <head>
-        <title>Roanoke College March Madness Pool</title>
-    </head>
+	<head>
+		<title>Roanoke College March Madness Pool</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	</head>
 	<style>
 		table {
 			border-collapse: collapse;
