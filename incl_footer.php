6c9aa12fc9b68cc0493378b179f1e2497d08574e
Phil Wright <wright.phillip@gmail.com>
2020-03-10 15:17:15 -0600

    Update footer to use a navbar

--- incl_footer.php
+++ incl_footer.php
@@ -1,12 +1,10 @@
-<br><br>
-
-        <table align="center" border="1">
-            <tr>
-                <td>
-                    Version 0.91; Copyright: David G. Taylor, 2015-<? echo date("Y"); ?>, with special thanks to Phil Wright.<br><center><a href="./index.php?action=admin">(Adminstrator Login)</a></center>
-                </td>
-            </tr>
-        </table>
-
-    </body>
-</html> 
+    <footer class="navbar">
+        <div class="navbar-nav navbar-left">
+            Version 0.91; Copyright: David G. Taylor, 2015-<? echo date("Y"); ?>, with special thanks to Phil Wright.
+        </div>
+        <a href="./index.php?action=admin">
+            (Adminstrator Login)
+        </a>
+    </footer>
+  </body>
+</html>
